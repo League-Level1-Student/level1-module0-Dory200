de40b084d633f795fbbd1fcffcc3df2960fddd70
tailw <tailw@DESKTOP-MR3PNPI>
2021-01-19 16:53:23 -0800

    finished duelling buttons

--- src/_05_body_part_quiz/BodyPartQuiz.java
+++ src/_05_body_part_quiz/BodyPartQuiz.java
@@ -42,7 +42,7 @@ public class BodyPartQuiz {
 
 		// 3. Change the size of the window so that you can only see part of the
 		// image.
-		window.setSize(500, 500);
+		window.setSize(200, 100);
 
 		showNextImage();
 
@@ -51,23 +51,55 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+		int score = 0;
 		// 2. Set the size of the window in the initializeGui() method 
-
+	    initializeGui();
 		// 4. Ask the user who this person is and store their answer
 		String guess = JOptionPane.showInputDialog("who is this?");
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
-
+		if(guess.equalsIgnoreCase("arnold")) {
+			JOptionPane.showMessageDialog(null, "you're right!");
+		     score++;
+		}
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
-
+		else {
+			JOptionPane.showMessageDialog(null, "you are wrong my brudda, it is actually ARNOLD");
+		}
 		// 7. Use the showNextImage() method below to get the next image
 		showNextImage();
 
 		// 8. .... repeat 4-7 for all your images.....
-
+		String gues = JOptionPane.showInputDialog("who is this?");
+		if(gues.equalsIgnoreCase("leonardo")) {
+			JOptionPane.showMessageDialog(null, "you're right!");
+		     score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "you are wrong my brudda, it is leonardo");
+		}
+		showNextImage();
+		String gue = JOptionPane.showInputDialog("who is this?");
+		if(gue.equalsIgnoreCase("morgan")) {
+			JOptionPane.showMessageDialog(null, "you're right!");
+		     score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "you are wrong my brudda, it is actually morgan");
+		}
+		showNextImage();
+		String guesss = JOptionPane.showInputDialog("who is this?");
+		if(guesss.equalsIgnoreCase("jack")) {
+			JOptionPane.showMessageDialog(null, "you're right!");
+		     score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "you are wrong my brudda, it is actually jack black");
+		}
+		
+		JOptionPane.showMessageDialog(null, score);
 		// 9. Show them their current score
 
 	}
